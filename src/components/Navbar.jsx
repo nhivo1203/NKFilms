5ae6e59f49521010d5ee4007ee5966041c542a83
trongtrung1411 <trungthang14112000@gmail.com>
2022-02-27 14:39:06 +0700

    đổi trang chính thành trang nhà

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -11,7 +11,7 @@ const Navbar = () => {
             src="https://upload.wikimedia.org/wikipedia/commons/thumb/0/08/Netflix_2015_logo.svg/2560px-Netflix_2015_logo.svg.png"
             alt=""
           />
-          <span>Trang chủ</span>
+          <span>Trang nhà</span>
           <span>Phim bộ</span>
           <span>Phim chiếu rạp</span>
           <span>Phim mới</span>
--- src/home/Home.jsx
+++ src/home/Home.jsx
@@ -5,7 +5,7 @@ import Navbar from "../components/Navbar";
 const Home = () => {
   return (
     <div className="home">
-      <Navbar />
+      <Navbar />{" "}
     </div>
   );
 };
